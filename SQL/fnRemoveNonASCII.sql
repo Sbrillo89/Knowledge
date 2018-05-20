536b4acf5687011ed71e125254579a58a2347341
Sbrillo89 <39387342+Sbrillo89@users.noreply.github.com>
2018-05-20 11:39:24 +0200

    Update and rename fnCleanString.sql to fnRemoveNonASCII.sql

---
+++ SQL/fnRemoveNonASCII.sql
@@ -0,0 +1,39 @@
+/*
+    Unicode & ASCII are the same from 1 to 255
+    Only Unicode goes beyond 255
+    0 to 31 are non-printable characters
+*/
+
+CREATE FUNCTION fnRemoveNonASCII
+(
+    @nstring nvarchar(255)
+)
+RETURNS varchar(255)
+AS
+BEGIN
+
+    DECLARE @Result varchar(255)
+    DECLARE @position int
+    DECLARE @nchar nvarchar(1)
+
+    SET @Result = ''
+    SET @position = 1
+    
+    WHILE @position <= LEN(@nstring)
+    BEGIN
+
+      SET @nchar = SUBSTRING(@nstring, @position, 1)
+      IF UNICODE(@nchar) between 32 and 255
+          SET @Result = @Result + @nchar
+
+      SET @position = @position + 1
+    END
+
+RETURN @Result
+
+END
+GO
+
+------------------------------------------------------------------------------------------------------
+
+
