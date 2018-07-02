4fa1d1be4dff39774eb0afea797d526cd621f795
Sbrillo89 <39387342+Sbrillo89@users.noreply.github.com>
2018-07-02 10:43:19 +0200

    Update Select_TableSpace.sql

--- SQL/Select_TableSpace.sql
+++ SQL/Select_TableSpace.sql
@@ -17,7 +17,8 @@ FROM sys.tables t
 INNER JOIN sys.indexes i
 	ON t.OBJECT_ID = i.object_id
 INNER JOIN sys.partitions p
-	ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+	ON i.object_id = p.OBJECT_ID 
+	AND i.index_id = p.index_id
 INNER JOIN sys.allocation_units a 
 	ON p.partition_id = a.container_id
 LEFT OUTER JOIN sys.schemas s 
