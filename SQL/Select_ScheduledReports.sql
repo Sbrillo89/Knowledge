079897b14783777d2ff019fe8f3995de1128279e
Sbrillo89 <39387342+Sbrillo89@users.noreply.github.com>
2018-07-04 08:45:47 +0200

    Create Select_ScheduledReports.sql

---
+++ SQL/Select_ScheduledReports.sql
@@ -0,0 +1,25 @@
+/*
+  Estrae dal report server le schedulazioni e il loro stato di esecuzione
+*/
+
+SELECT 
+   Schedule.ScheduleID 			          AS SQLAgent_Job_Name
+   ,Subscriptions.Description 		    AS sub_desc
+   ,Subscriptions.DeliveryExtension 	AS sub_delExt
+   ,Subscriptions.LastRunTime
+   ,Subscriptions.EventType
+   ,Subscriptions.LastStatus
+   ,Subscriptions.DeliveryExtension
+   ,[Catalog].Name 			              AS ReportName
+
+FROM reportserver.dbo.ReportSchedule 
+
+INNER JOIN reportserver.dbo.Schedule
+	  ON ReportSchedule.ScheduleID = Schedule.ScheduleID
+INNER JOIN reportserver.dbo.Subscriptions
+	  ON ReportSchedule.SubscriptionID = Subscriptions.SubscriptionID
+INNER JOIN reportserver.dbo.[Catalog]
+	  ON ReportSchedule.ReportID = [Catalog].ItemID
+	  AND Subscriptions.Report_OID = [Catalog].ItemID
+  
+  
