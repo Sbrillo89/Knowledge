ac75235d0e4452ff59dd83596c99925824245d6c
Sbrillo89 <39387342+Sbrillo89@users.noreply.github.com>
2018-09-05 12:53:29 +0200

    Update Select_ScheduledReports.sql

--- SQL/Select_ScheduledReports.sql
+++ SQL/Select_ScheduledReports.sql
@@ -3,14 +3,14 @@
 */
 
 SELECT 
-   Schedule.ScheduleID 			          AS SQLAgent_Job_Name
-   ,Subscriptions.Description 		    AS sub_desc
+   Schedule.ScheduleID 			AS SQLAgent_Job_Name
+   ,Subscriptions.Description 		AS sub_desc
    ,Subscriptions.DeliveryExtension 	AS sub_delExt
    ,Subscriptions.LastRunTime
    ,Subscriptions.EventType
    ,Subscriptions.LastStatus
    ,Subscriptions.DeliveryExtension
-   ,[Catalog].Name 			              AS ReportName
+   ,[Catalog].Name 			AS ReportName
 
 FROM reportserver.dbo.ReportSchedule 
 
@@ -21,5 +21,4 @@ INNER JOIN reportserver.dbo.Subscriptions
 INNER JOIN reportserver.dbo.[Catalog]
 	  ON ReportSchedule.ReportID = [Catalog].ItemID
 	  AND Subscriptions.Report_OID = [Catalog].ItemID
-  
   
